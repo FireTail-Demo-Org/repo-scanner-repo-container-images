31dae6f6edf99c14b90fe5da1ec0c298695f82b9
Greg Sheppard <gregalia@users.noreply.github.com>
2024-07-25 15:49:41 +0300

    feat: Add deployment files with container images
    https://firetail-io.atlassian.net/browse/FIRE-2552

--- build/build-deploy.sh
+++ build/build-deploy.sh
@@ -3,19 +3,43 @@
 set -o errexit
 set -o nounset
 
-docker buildx build \
-  --target runtime-image \
-  --tag repo:v1.0 \
-  --file build/Dockerfile \
+docker buildx build --load \
+  --target test-image \
+  --file build/dockerfile \
+  --tag app:test-image \
   .
 
-docker image tag repo:v1.0 example.com/repo:v1.0
+docker container run --rm \
+  --tag app:test-image
 
-echo "${PASSWORD}" |
+docker buildx build --load \
+  --target runtime-image \
+  --file build/dockerfile \
+  --tag app:latest \
+  .
+
+aws ecr get-login-password |
   docker login \
-    --username AWS \
-    --password-stdin \
-    example.com
+  --username AWS \
+  --password-stdin \
+  01234567890.dkr.ecr.eu-west-1.amazonaws.com
 
-# this is a comment about docker push
-docker push example.com/repo:v1.0
+docker image tag \
+  app:latest \
+  01234567890.dkr.ecr.eu-west-1.amazonaws.com/app:v1.2.3
+
+docker tag \
+  app:latest \
+  01234567890.dkr.ecr.eu-west-1.amazonaws.com/app:latest
+
+docker image push \
+  01234567890.dkr.ecr.eu-west-1.amazonaws.com/app:latest
+
+docker push \
+  01234567890.dkr.ecr.eu-west-1.amazonaws.com/app:v1.2.3
+
+docker tag app:latest example.com/app:latest
+
+docker push example.com/app:latest
+
+docker tag app:latest example.com/app:v1.2.3
